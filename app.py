047f3c8be0644fe37e3109924c2d850fb01e2942
Charles Zeng <judiciousness@gmail.com>
2018-12-25 00:06:52 -0500

    add schedule.py

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 #!flask/bin/python
+from __future__ import print_function
 from flask import Flask
+from ortools.linear_solver import pywraplp
+
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "Hello, World!"
+    solver = pywraplp.Solver('SolveSimpleSystem',
+                           pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    # Create the variables x and y.
+    x = solver.NumVar(0, 1, 'x')
+    y = solver.NumVar(0, 2, 'y')
+    # Create the objective function, x + y.
+    objective = solver.Objective()
+    objective.SetCoefficient(x, 1)
+    objective.SetCoefficient(y, 1)
+    objective.SetMaximization()
+    # Call the solver and display the results.
+    solver.Solve()
+    print('Solution:')
+    print('x = ', x.solution_value())
+    print('y = ', y.solution_value())
+    return 'x = ' + str(x.solution_value()) + 'y = ' + str(y.solution_value())
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
+    # app.run(host='192.168.1.21', port=5010)
