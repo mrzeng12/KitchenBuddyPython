209df4964be5afb0978385e6834f87032b7eb6fa
e049652 <e049652@mastercard.com>
2018-12-28 16:36:23 -0500

    add more rules

--- meal.py
+++ meal.py
@@ -7,12 +7,6 @@ from ortools.constraint_solver import pywrapcp
 def main():
     with open('data.json', encoding='utf8') as json_data:
         data = json.load(json_data)
-        # print(d)
-    # vegi_dish_list = [item["name"] for item in data if item["vegitable"]]
-    # meet_dish_list = [item["name"] for item in data if item["pork"]]
-    # soup_dish_list = [item["name"] for item in data if item["soup"]]
-
-    # print(vegi_dish)
 
     # Creates the solver.
     solver = pywrapcp.Solver("meal_solver")
@@ -36,17 +30,43 @@ def main():
 
     #Each week one dish is fish
     fish_dish_list = [i for i in range(len(data)) if data[i]["fish"]]
-    print(fish_dish_list)
 
     solver.Add(solver.Sum([result_dishes[j,k] == fish_dish_list[i] for i in range(len(fish_dish_list)) for j in range(num_meals) for k in range(num_dishes)]) == 1)
 
     #Each week one dish is bean
 
     bean_dish_list = [i for i in range(len(data)) if data[i]["bean"]]
-    print(bean_dish_list)
 
     solver.Add(solver.Sum([result_dishes[j,k] == bean_dish_list[i] for i in range(len(bean_dish_list)) for j in range(num_meals) for k in range(num_dishes)]) == 1)
 
+
+    #Each day one dish is soup
+    soup_dish_list = [i for i in range(len(data)) if data[i]["soup"]]
+
+    for j in range(num_meals):
+      solver.Add(solver.Sum([result_dishes[j,k] == soup_dish_list[i] for i in range(len(soup_dish_list)) for k in range(num_dishes)]) == 1)
+
+    #Each day one dish is meat (pork, chicken, beef, fish, seafood)
+    meat_dish_list = [i for i in range(len(data)) if data[i]["pork"] or data[i]["chicken"] or data[i]["beef"] or data[i]["fish"] or data[i]["seafood"]]
+
+    # for j in range(num_meals):
+      # solver.Add(solver.Sum([result_dishes[j,k] == meat_dish_list[i] for i in range(len(meat_dish_list)) for k in range(num_dishes)]) == 1)
+
+    #Each day one dish is pure vegi
+    pure_vegi_dish_list = [i for i in range(len(data)) if data[i]["vegitable"] and i not in meat_dish_list and i not in soup_dish_list]
+    
+    for j in range(num_meals):
+      solver.Add(solver.Sum([result_dishes[j,k] == pure_vegi_dish_list[i] for i in range(len(pure_vegi_dish_list)) for k in range(num_dishes)]) == 1)
+
+    #Each day up to one dish is pork
+    pork_dish_list = [i for i in range(len(data)) if data[i]["pork"]]
+    for j in range(num_meals):
+      solver.Add(solver.Sum([result_dishes[j,k] == pork_dish_list[i] for i in range(len(pork_dish_list)) for k in range(num_dishes)]) == 1)
+
+    #Each day up to one dish is chicken
+    egg_dish_list = [i for i in range(len(data)) if data[i]["egg"]]
+    for j in range(num_meals):
+      solver.Add(solver.Sum([result_dishes[j,k] == egg_dish_list[i] for i in range(len(egg_dish_list)) for k in range(num_dishes)]) == 1)  
 
     # Create the decision builder.
     decision_builder = solver.Phase(dishes_flat, solver.CHOOSE_FIRST_UNBOUND,
@@ -68,10 +88,22 @@ def main():
     # solver.NextSolution()
     # print(dishes_flat)
     for i in range(num_meals):
-      print("Meal", i)
+      print("Meal", i+1)
+      print_vegi_dish = -1
+      print_meat_dish = -1
+      print_soup_dish = -1
       for j in range(num_dishes):
         dish_index = collector.Value(0,result_dishes[(i,j)])
-        print("Dish", data[dish_index]["name"])
+        if dish_index in pure_vegi_dish_list:
+          print_vegi_dish = dish_index
+        elif dish_index in soup_dish_list:
+          print_soup_dish = dish_index
+        else:
+          print_meat_dish = dish_index
+        
+      print("Vegi", data[print_vegi_dish]["name"])
+      print("Meat", data[print_meat_dish]["name"])
+      print("Soup", data[print_soup_dish]["name"])
       print()
 
     # solver.NextSolution()
